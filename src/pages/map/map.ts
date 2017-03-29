885883179f099031089ab6371e9cdffd3655d56c
-KaiK- <enriqueoriol@gmail.com>
2017-03-29 06:34:24 +0200

    16 - Adding geolocation marker

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams, Platform } from 'ionic-angular';
 import { GoogleMaps, GoogleMap, GoogleMapsEvent, LatLng } from '@ionic-native/google-maps';
+import { Geolocation } from '@ionic-native/geolocation';
 
 /*
   Generated class for the Map page.
@@ -11,13 +12,13 @@ import { GoogleMaps, GoogleMap, GoogleMapsEvent, LatLng } from '@ionic-native/go
 @Component({
   selector: 'page-map',
   templateUrl: 'map.html',
-  providers: [GoogleMaps]
+  providers: [GoogleMaps, Geolocation]
 })
 export class MapPage {
 
   public map: GoogleMap;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public googleMaps: GoogleMaps, private platform: Platform) {}
+  constructor(public navCtrl: NavController, public navParams: NavParams, public googleMaps: GoogleMaps, private platform: Platform, private geolocation: Geolocation) {}
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad MapPage');
@@ -27,8 +28,14 @@ export class MapPage {
         this.map = this.googleMaps.create(element);
         this.map.one(GoogleMapsEvent.MAP_READY).then((data:any)=>{
           //let's center map based on our position
-          let myPosition: LatLng = new LatLng(41.390295, 2.154007);
-          this.map.animateCamera({target: myPosition,zoom: 10});
+          this.geolocation.getCurrentPosition().then(pos => {
+            let myPosition = new LatLng(pos.coords.latitude, pos.coords.longitude);
+            this.map.animateCamera({target: myPosition,zoom: 10});
+            this.map.addMarker({
+              position: myPosition,
+              title: 'You are here'
+            });
+          })
         })
       }).catch(()=>alert("GoogleMap is not available"));
     })
